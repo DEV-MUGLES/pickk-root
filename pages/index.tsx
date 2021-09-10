3325a561fe469160119397bafd7cd591da6a0a07
greatSumini <greatSumini@gmail.com>
2021-09-10 18:43:57 +0900

    feat: add link for images-upload page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link';
+
 export default function HomePage() {
-  return <div>안녕 나는 수민</div>;
+  return (
+    <div>
+      <h1>루트 어드민 페이지에 당도한 것을 환영하오</h1>
+      <Link href="/images-upload">이미지업로드하러가기</Link>
+    </div>
+  );
 }
