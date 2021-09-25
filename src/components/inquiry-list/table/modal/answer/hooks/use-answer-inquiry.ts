4ce74d1f043f01bce88f548074cd55e6c6142cd1
syoung125 <laura120555@gmail.com>
2021-09-25 18:17:10 +0900

    feat: add useAnswerInquiry hook

---
+++ src/components/inquiry-list/table/modal/answer/hooks/use-answer-inquiry.ts
@@ -0,0 +1,43 @@
+import { gql, useMutation } from '@apollo/client';
+import { Mutation, MutationRootAnswerInquiryArgs } from '@pickk/common';
+
+const ANSWER_INQUIRY = gql`
+  mutation rootAnswerInquiry(
+    $id: Int!
+    $answerInquiryInput: AnswerInquiryInput!
+  ) {
+    rootAnswerInquiry(id: $id, answerInquiryInput: $answerInquiryInput) {
+      id
+      answers {
+        id
+        content
+        displayAuthor
+      }
+    }
+  }
+`;
+
+export const useAnswerInquiry = () => {
+  const [answer] = useMutation<
+    Pick<Mutation, 'rootAnswerInquiry'>,
+    MutationRootAnswerInquiryArgs
+  >(ANSWER_INQUIRY);
+
+  const answerInquiry = async (
+    id: number,
+    content: string,
+    displayAuthor: string
+  ) => {
+    await answer({
+      variables: {
+        id,
+        answerInquiryInput: {
+          content,
+          displayAuthor,
+        },
+      },
+    });
+  };
+
+  return { answerInquiry };
+};
