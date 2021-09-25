53d8fd525b369f3a21ae81a1d84141c27390a867
syoung125 <laura120555@gmail.com>
2021-09-25 16:14:17 +0900

    chore: set dayjs relativeTime, locale ko

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,15 @@
 import App, { AppContext, AppProps } from 'next/app';
 import { ApolloProvider, gql } from '@apollo/client';
+import dayjs from 'dayjs';
+import relativeTime from 'dayjs/plugin/relativeTime';
+import 'dayjs/locale/ko';
 import 'antd/dist/antd.css';
 import { UserRole } from '@pickk/common';
 
 import { createApolloClient, useApolloClient } from '@providers/apollo';
+
+dayjs.extend(relativeTime);
+dayjs.locale('ko');
 
 export default function AdminApp({ Component, pageProps }: AppProps) {
   const apolloClient = useApolloClient(pageProps);
