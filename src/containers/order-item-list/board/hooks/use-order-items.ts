0814378968551e271e0ce7836a0185b96f497125
syoung125 <laura120555@gmail.com>
2021-09-27 13:12:05 +0900

    feat: :sparkles: add useOrderItems hook

---
+++ src/containers/order-item-list/board/hooks/use-order-items.ts
@@ -0,0 +1,83 @@
+import { gql, useQuery } from '@apollo/client';
+import {
+  OrderItem,
+  Order,
+  OrderBuyer,
+  OrderReceiver,
+  QueryRootOrderItemsArgs,
+} from '@pickk/common';
+
+import { BoardTemplateProps } from '@components/common/template';
+
+const GET_ORDER_ITEMS = gql`
+  query rootOrderItems($orderItemFilter: OrderItemFilter) {
+    rootOrderItems(orderItemFilter: $orderItemFilter) {
+      id
+      merchantUid
+      orderMerchantUid
+      createdAt
+      status
+      claimStatus
+      itemId
+      itemName
+      productVariantName
+      quantity
+      order {
+        id
+        buyer {
+          id
+          name
+          phoneNumber
+        }
+        receiver {
+          id
+          name
+        }
+      }
+    }
+  }
+`;
+
+export type OrderItemDataType = Pick<
+  OrderItem,
+  | 'id'
+  | 'merchantUid'
+  | 'orderMerchantUid'
+  | 'createdAt'
+  | 'status'
+  | 'claimStatus'
+  | 'itemId'
+  | 'itemName'
+  | 'productVariantName'
+  | 'quantity'
+> & {
+  order: Pick<Order, 'id'> & {
+    buyer: Pick<OrderBuyer, 'id' | 'name' | 'phoneNumber'>;
+    receiver: Pick<OrderReceiver, 'id' | 'name'>;
+  };
+};
+
+export const useOrderItems: BoardTemplateProps['useBoardData'] = ({
+  filter,
+}) => {
+  const {
+    data,
+    loading,
+    refetch: _refetch,
+  } = useQuery<
+    { rootOrderItems: OrderItemDataType[] },
+    QueryRootOrderItemsArgs
+  >(GET_ORDER_ITEMS, {
+    variables: {
+      orderItemFilter: filter,
+    },
+  });
+
+  const refetch = async () => {
+    await _refetch({
+      orderItemFilter: filter,
+    });
+  };
+
+  return { data: data?.rootOrderItems, loading, refetch };
+};
