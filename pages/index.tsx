2ebeef5189d21a93277d9efe2bca4b131711a3ed
greatSumini <greatSumini@gmail.com>
2021-10-14 05:18:22 +0900

    feat(pages): :sparkles: add ItemPage

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,6 +26,7 @@ export default function HomePage({ me }: HomePageProps) {
         <Link href="/images-upload">🖼 이미지업로드</Link>
         <Link href="/inquiries">💬 문의내역</Link>
         <Link href="/order-items">🛍 주문상품내역</Link>
+        <Link href="/item">👕 (임시) 상품 업데이트</Link>
       </StyledMenuWrapper>
     </div>
   );
---
+++ pages/item.tsx
@@ -0,0 +1,62 @@
+import { useState } from 'react';
+import styled from 'styled-components';
+import { useMutation, gql } from '@apollo/client';
+import { Button, InputNumber, message } from 'antd';
+
+const StyledWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  width: fit-content;
+`;
+
+const StyledA = styled.a`
+  font-size: 2rem;
+  color: blue;
+  text-decoration: underline;
+`;
+
+export default function ItemPage() {
+  const [updateRootItemImageUrl] = useMutation(gql`
+    mutation updateRootItemImageUrl($itemId: Int!) {
+      updateRootItemImageUrl(itemId: $itemId) {
+        id
+      }
+    }
+  `);
+  const [updateRootItemDetailImages] = useMutation(gql`
+    mutation updateRootItemDetailImages($itemId: Int!) {
+      updateRootItemDetailImages(itemId: $itemId) {
+        id
+      }
+    }
+  `);
+
+  const [itemId, setItemId] = useState(null);
+
+  const updateImageUrl = async () => {
+    await updateRootItemImageUrl({ variables: { itemId } });
+    message.success('업데이트되었습니다.');
+  };
+
+  const updateDetailImages = async () => {
+    await updateRootItemDetailImages({ variables: { itemId } });
+    message.success('업데이트되었습니다.');
+  };
+
+  return (
+    <StyledWrapper>
+      아래에 아이템 ID를 입력하세요
+      <InputNumber min={1} value={itemId} onChange={setItemId} />
+      <Button type="primary" disabled={!itemId} onClick={updateImageUrl}>
+        대표 이미지 업데이트
+      </Button>
+      <Button type="primary" disabled={!itemId} onClick={updateDetailImages}>
+        상세 이미지들 업데이트
+      </Button>
+      <StyledA target="_blank" href={`https://pickk.one/items/${itemId}`}>
+        핔 링크 (https://pickk.one/items/{itemId})
+      </StyledA>
+    </StyledWrapper>
+  );
+}
