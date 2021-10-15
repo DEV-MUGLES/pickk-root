baf6b63af8a51418a657c34e1b5898b3aae55327
syoung125 <laura120555@gmail.com>
2021-10-15 14:26:54 +0900

    feat: add sellable-items, items menu to HomePage

--- pages/index.tsx
+++ pages/index.tsx
@@ -26,7 +26,9 @@ export default function HomePage({ me }: HomePageProps) {
         <Link href="/images-upload">🖼 이미지업로드</Link>
         <Link href="/inquiries">💬 문의내역</Link>
         <Link href="/order-items">🛍 주문상품내역</Link>
-        <Link href="/item">👕 (임시) 상품 업데이트</Link>
+        <Link href="/sellable-items">👕 활성상품 관리</Link>
+        <Link href="/items">👖 전체상품 관리</Link>
+        <Link href="/item">✋ (임시) 상품 업데이트</Link>
       </StyledMenuWrapper>
     </div>
   );
