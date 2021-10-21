3aa30286a30b0ee4b84cd7ac826a22ed7d60d2df
syoung125 <laura120555@gmail.com>
2021-10-21 16:09:02 +0900

    feat: :sparkles: add board filter formatting logic

--- src/components/common/templates/board/board.tsx
+++ src/components/common/templates/board/board.tsx
@@ -6,6 +6,9 @@ import { palette } from '@pickk/design-token';
 
 import { BoardFilter, BoardTable } from '@components/common/organisms';
 
+import { DatePickerValueType } from '@components/common/molecules/form-inputs/date-picker';
+import { removeDashFromNumber } from '@common/helpers';
+
 import { BoardTemplateProps } from './board.type';
 
 const StyledWrapper = styled.div`
@@ -32,12 +35,45 @@ export default function BoardTemplate(props: BoardTemplateProps) {
   const [filter, setFilter] = useState<Record<string, unknown>>(defaultFilter);
   const [query, setQuery] = useState(null);
 
-  const handleFilterChange = (newFilter: Record<string, unknown>) => {
-    /** query 필드는 filter에서 제외한다. */
-    setQuery(newFilter.query ?? null);
-    delete newFilter.query;
+  const formatFilter = (
+    inputs: Record<string, unknown>
+  ): Record<string, unknown> => {
+    let result = { ...inputs };
 
-    setFilter(newFilter);
+    /**  조회 기간 필터를 형식에 맞게 변경한다. */
+    const datePeriodFilter = inputs.period as DatePickerValueType;
+    result = {
+      ...result,
+      ...(datePeriodFilter
+        ? {
+            [datePeriodFilter['lookup']]: datePeriodFilter['range'],
+          }
+        : {}),
+    };
+    delete result['period'];
+
+    /**  검색어가 숫자와 '-'의 조합인 경우 '-' 를 제거한다.  */
+    result = {
+      ...result,
+      ...(result['search']
+        ? { search: removeDashFromNumber(result['search'] as string) }
+        : {}),
+      ...(result['query']
+        ? { query: removeDashFromNumber(result['query'] as string) }
+        : {}),
+    };
+
+    return result;
+  };
+
+  const handleFilterChange = (newFilter: Record<string, unknown>) => {
+    const formattedFilter = formatFilter(newFilter);
+
+    /** query 필드는 filter에서 제외한다. */
+    setQuery(formattedFilter.query ?? null);
+    delete formattedFilter.query;
+
+    setFilter(formattedFilter);
   };
 
   return (
