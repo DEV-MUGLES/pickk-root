d518bb53de780e79fc347275d203572aa2f830ca
greatSumini <greatSumini@gmail.com>
2021-11-01 04:46:29 +0900

    feat: :sparkles: add ConfirmedAtBetweenDatePicker

---
+++ src/components/confirmed-order-item-list/filter/confirmed-at-between.tsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import { RangePicker } from '@components/common/molecules';
+
+export default function ConfirmedAtBetweenDatePicker(props: any) {
+  return <RangePicker {...props} />;
+}
--- src/containers/confirmed-order-item-list/board/board.tsx
+++ src/containers/confirmed-order-item-list/board/board.tsx
@@ -1,6 +1,6 @@
 import { BoardTemplate } from '@components/common/templates';
 import { confirmedOrderItemsTableColumns } from '@components/confirmed-order-item-list';
-import { orderItemsFilterInputs } from '@components/order-item-list';
+import { confirmedOrderItemsFilterInputs } from '@components/confirmed-order-item-list';
 
 import { useConfirmedOrderItems } from './hooks';
 
@@ -11,7 +11,7 @@ export default function OrderItemListBoardContainer() {
       subTitle="주문상품내역을 볼 수 있습니다."
       useTableData={useConfirmedOrderItems}
       columns={confirmedOrderItemsTableColumns}
-      filterInputs={orderItemsFilterInputs}
+      filterInputs={confirmedOrderItemsFilterInputs}
     />
   );
 }
