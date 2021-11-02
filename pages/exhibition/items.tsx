f64d9cdb62c439dbc31d42a861b0be31c7bc76fa
greatSumini <greatSumini@gmail.com>
2021-11-02 18:51:02 +0900

    feat: add create button to ItemsExhibitionPage

--- pages/exhibition/items.tsx
+++ pages/exhibition/items.tsx
@@ -1,8 +1,22 @@
+import Link from 'next/link';
+
 import { ItemsExhibitionList } from '@containers/items-exhibition';
 
 export default function ItemsExhibitionPage() {
   return (
     <>
+      <Link href="/exhibition/items/edit">
+        <a
+          style={{
+            fontSize: 20,
+            border: '1px solid black',
+            color: 'red',
+            margin: '20px',
+          }}
+        >
+          새 기획전 추가
+        </a>
+      </Link>
       <ItemsExhibitionList />
     </>
   );
