7e8961d584cd8bc38eaabd085427b7e601867d0a
greatSumini <greatSumini@gmail.com>
2021-11-02 18:45:52 +0900

    feat(pages): :sparkles: add ItemsExhibitionPage

---
+++ src/containers/items-exhibition/list/hooks/use-item-exhibitions.ts
@@ -0,0 +1,46 @@
+import { gql, useQuery } from '@apollo/client';
+import { ItemsExhibition } from '@pickk/common';
+
+const GET_ITEMS_EXHIBITIONS = gql`
+  query itemsExhibitions {
+    itemsExhibitions {
+      id
+      title
+      description
+
+      imageUrl
+      imageTop
+      imageRight
+      backgroundColor
+
+      order
+      isVisible
+
+      items {
+        id
+        name
+        imageUrl
+
+        originalPrice
+        finalPrice
+
+        isSoldout
+
+        brand {
+          id
+          nameKor
+        }
+      }
+    }
+  }
+`;
+
+export const useItemsExhibitions = () => {
+  const { data } = useQuery<{ itemsExhibitions: ItemsExhibition[] }>(
+    GET_ITEMS_EXHIBITIONS
+  );
+
+  return {
+    data: data?.itemsExhibitions,
+  };
+};
