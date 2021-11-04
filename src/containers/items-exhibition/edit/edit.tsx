85edcce4074013d01f299d52349e4fdb16351d18
greatSumini <greatSumini@gmail.com>
2021-11-04 11:36:32 +0900

    feat: :lipstick: update ItemsExhibitionEditContainer layout

--- src/containers/items-exhibition/edit/edit.tsx
+++ src/containers/items-exhibition/edit/edit.tsx
@@ -31,70 +31,78 @@ export default function ItemsExhibitionEditContainer({ id }: { id?: number }) {
 
   return (
     <div className={styles.wrapper}>
-      <h2>추가/수정</h2>
-      <h4>제모옥은</h4>
-      <textarea name="title" value={form.title} onChange={handleInputChange} />
-      <h4>설명</h4>
-      <textarea
-        name="description"
-        value={form.description}
-        onChange={handleInputChange}
-      />
-      <h4>이미지 URL</h4>
-      <input
-        name="imageUrl"
-        value={form.imageUrl}
-        onChange={handleInputChange}
-      />
-      <h4>이미지 상단 이동</h4>
-      <input
-        type="number"
-        name="imageTop"
-        value={form.imageTop}
-        onChange={handleInputChange}
-      />
-      <h4>이미지 우측 이동</h4>
-      <input
-        type="number"
-        name="imageRight"
-        value={form.imageRight}
-        onChange={handleInputChange}
-      />
-      <h4>배경색</h4>
-      <SketchPicker
-        color={form.backgroundColor ?? '#fff'}
-        onChange={(color) => {
-          handleFieldChange('backgroundColor', color?.hex);
-        }}
-      />
-      <br />
-      <h4>노출여부</h4>
-      <input
-        type="checkbox"
-        name="isVisible"
-        checked={form.isVisible}
-        onChange={handleInputChange}
-      />
-      <h4>순서</h4>
-      <input
-        type="number"
-        name="order"
-        value={form.order}
-        onChange={handleInputChange}
-      />
-      <h4>아이템ID들</h4>
-      <input
-        name="itemIdsStr"
-        value={form.itemIdsStr}
-        onChange={handleInputChange}
-      />
-      <h2>미리보기</h2>
-      <ItemsExhibitionCard
-        {...form}
-        items={form.itemIds.map((id) => ({ id }))}
-      />
-      <br />
-      <button onClick={handleSubmit}>저장하기</button>
+      <div className={styles.edit}>
+        <h2>추가/수정</h2>
+        <h4>제모옥은</h4>
+        <textarea
+          name="title"
+          value={form.title}
+          onChange={handleInputChange}
+        />
+        <h4>설명</h4>
+        <textarea
+          name="description"
+          value={form.description}
+          onChange={handleInputChange}
+        />
+        <h4>이미지 URL</h4>
+        <input
+          name="imageUrl"
+          value={form.imageUrl}
+          onChange={handleInputChange}
+        />
+        <h4>이미지 상단 이동</h4>
+        <input
+          type="number"
+          name="imageTop"
+          value={form.imageTop}
+          onChange={handleInputChange}
+        />
+        <h4>이미지 우측 이동</h4>
+        <input
+          type="number"
+          name="imageRight"
+          value={form.imageRight}
+          onChange={handleInputChange}
+        />
+        <h4>배경색</h4>
+        <SketchPicker
+          color={form.backgroundColor ?? '#fff'}
+          onChange={(color) => {
+            handleFieldChange('backgroundColor', color?.hex);
+          }}
+        />
+        <br />
+        <h4>노출여부</h4>
+        <input
+          type="checkbox"
+          name="isVisible"
+          checked={form.isVisible}
+          onChange={handleInputChange}
+        />
+        <h4>순서</h4>
+        <input
+          type="number"
+          name="order"
+          value={form.order}
+          onChange={handleInputChange}
+        />
+        <h4>아이템ID들</h4>
+        <input
+          name="itemIdsStr"
+          value={form.itemIdsStr}
+          onChange={handleInputChange}
+        />
+        <br />
+        <button onClick={handleSubmit}>저장하기</button>
+      </div>
+      <div>
+        <h2>미리보기</h2>
+        <ItemsExhibitionCard
+          {...form}
+          items={form.itemIds.map((id) => ({ id }))}
+        />
+      </div>
     </div>
   );
 }
